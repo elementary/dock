cfee7c17df254ce7aac15f41a5db4ccf7f1c2a3c
Cassidy James Blaede <c@ssidyjam.es>
2020-07-28 22:15:38 -0600

    “Let the past die; kill it if you have to” –Kylo Ren

--- docs/install_docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gtk-doc/html/"
-cp -r "${MESON_BUILD_ROOT}/docs/vala-doc/plank" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/gtk-doc/html/"
--- tests/headless_runner.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xvfb-run --auto-servernum --server-args="-screen 0 1280x1024x24" dbus-run-session "$1"
